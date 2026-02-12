694473e3fe76b68a5e8fefe41843b2bbd41e6541
xiaohui.liu <xliu715@gmail.com>
2026-02-12 16:16:05 +0800

    Add services folder structure

--- src/models.ts
+++ src/models.ts
@@ -1,1 +1,14 @@
 //placeholder
+export type Activity = {
+  id: string;
+  name: string;
+  cost: number;
+  category: "food" | "transport" | "sightseeing";
+  startTime: Date;
+};
+export type Trip = {
+  id: string;
+  destination: string;
+  startDate: Date;
+  activities: Activity[];
+};
---
+++ src/services/destinationService.ts
@@ -0,0 +1,17 @@
+export const getDestinationInfo = async (countryName: string) => {
+  try {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/name/${countryName}`,
+    );
+    const data = await response.json();
+    const result = {
+      currency: Object.keys(data[0].currencies)[0],
+      flag: data[0].flag,
+    };
+    console.log(result);
+  } catch (error) {
+    throw new Error("Could not fetch country data");
+  }
+};
+
+getDestinationInfo("United Kingdom");
