ca6013222fab0e94c56219661b1654b9e4e29830
Ernest Dogo <dogoernest@outlook.com>
2026-02-17 11:30:21 +0100

    Export activity functions and remove debug code
    Closes #6

--- src/services/activityService.ts
+++ src/services/activityService.ts
@@ -9,7 +9,7 @@ let activities: Activity[] = dbData.trips[0].activities.map(activity => ({
     category: activity.category as "food" | "transport" | "sightseeing"
 }));
 
-const addActivity = (
+export const addActivity = (
     name: string, 
     cost: number, 
     category: "food" | "transport" | "sightseeing", 
@@ -28,7 +28,7 @@ const addActivity = (
 
 
 
-const getActivitiesByDay = (date: Date): Activity[] =>{
+export const getActivitiesByDay = (date: Date): Activity[] =>{
     const activitiesOnDay: Activity[] =  []
     for (const activity of activities){
         if (activity.startTime.toDateString() === date.toDateString() ){
@@ -38,7 +38,7 @@ const getActivitiesByDay = (date: Date): Activity[] =>{
     return activitiesOnDay;
 };
 
-const getActivitiesByCategory = (category: "food" | "transport" | "sightseeing"): Activity[] =>{
+export const getActivitiesByCategory = (category: "food" | "transport" | "sightseeing"): Activity[] =>{
     const filteredActivities: Activity[] =  []
     for (const activity of activities){
         if (activity.category === category ){
@@ -48,21 +48,9 @@ const getActivitiesByCategory = (category: "food" | "transport" | "sightseeing")
     return filteredActivities;
 };
 
-const getActivitiesChronologically = (): Activity[] =>{
+export const getActivitiesChronologically = (): Activity[] =>{
     return [...activities].sort((a, b ) =>{
         return compareAsc((a.startTime), (b.startTime))
     });
 };
 
-// console.log(getActivitiesChronologically());
-// addActivity("Testing", 500, "food", new Date("2026-06-30"));
-// console.log(getActivitiesByDay( new Date("2026-06-30")));
-console.log(getActivitiesByCategory("transport"));
-
-
-
-
-//addActivity();
-//getActivitiesByDay();
-//getActivitiesByCategory();
-//getAllCategoriesChronologically(); *
